ce1df28efb9c6b9961c96a44be30aefdb21f55f4
ElkaKR <ponomarenkoalena555@gmail.com>
2023-02-07 16:05:32 +0200

    Add PetClinic Index page and Controller. Closes #6

---
+++ pet-clinic-web/src/main/kotlin/learn/sfgpetclinic/controllres/IndexController.kt
@@ -0,0 +1,12 @@
+package learn.sfgpetclinic.controllres
+
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class IndexController {
+    @GetMapping("","index","index.html")
+    fun index():String{
+        return "index"
+    }
+}
