914332d8ec3e5074e15cb389c25b08fc27b47929
ElkaKR <ponomarenkoalena555@gmail.com>
2023-02-11 16:43:53 +0200

    list all vets. Closes #14

--- pet-clinic-web/src/main/kotlin/learn/sfgpetclinic/controllres/VetController.kt
+++ pet-clinic-web/src/main/kotlin/learn/sfgpetclinic/controllres/VetController.kt
@@ -1,12 +1,18 @@
 package learn.sfgpetclinic.controllres
 
+import learn.sfgpetclinic.services.VetService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class VetController {
+    @Autowired
+    lateinit var vetService: VetService
     @GetMapping("/vets","/vets/index","/vets/index.html")
-    fun listVets():String{
+    fun listVets(model: Model):String{
+        model.addAttribute("vets", vetService.findAll())
         return "vets/index"
     }
 }
