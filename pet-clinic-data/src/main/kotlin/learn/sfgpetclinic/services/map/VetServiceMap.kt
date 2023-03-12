209ce5a238c65b794a408f3a5eae1cc412c29cb4
ElkaKR <ponomarenkoalena555@gmail.com>
2023-03-12 20:01:48 +0200

    Create Specialities, add to Vets on Startup (Bootstrap). Closes #33

--- pet-clinic-data/src/main/kotlin/learn/sfgpetclinic/services/map/VetServiceMap.kt
+++ pet-clinic-data/src/main/kotlin/learn/sfgpetclinic/services/map/VetServiceMap.kt
@@ -1,16 +1,27 @@
 package learn.sfgpetclinic.services.map
 
 import learn.sfgpetclinic.model.Vet
+import learn.sfgpetclinic.services.SpecialityService
 import learn.sfgpetclinic.services.VetService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
 class VetServiceMap:AbstractMapService<Vet, Long>(), VetService {
+    @Autowired
+    lateinit var specialtyService:SpecialityService
     override fun findByID(id: Long): Vet {
        return this.findByID(id)
     }
 
     override fun deleteById(id: Long) {
         return super.deleteByID(id)
+    }
+
+    override fun save(data: Vet): Vet {
+        data.specialities.forEach {
+            if (it.id == null) specialtyService.save(it)
+        }
+        return super.save(data)
     }
 }
--- pet-clinic-web/src/main/kotlin/learn/sfgpetclinic/bootstrap/DataLoader.kt
+++ pet-clinic-web/src/main/kotlin/learn/sfgpetclinic/bootstrap/DataLoader.kt
@@ -1,11 +1,9 @@
 package learn.sfgpetclinic.bootstrap
 
-import learn.sfgpetclinic.model.Owner
-import learn.sfgpetclinic.model.Pet
-import learn.sfgpetclinic.model.PetType
-import learn.sfgpetclinic.model.Vet
+import learn.sfgpetclinic.model.*
 import learn.sfgpetclinic.services.OwnerService
 import learn.sfgpetclinic.services.PetTypeService
+import learn.sfgpetclinic.services.SpecialityService
 import learn.sfgpetclinic.services.VetService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
@@ -16,13 +14,22 @@ import java.time.LocalDate
 class DataLoader : CommandLineRunner {
     @Autowired
     lateinit var ownerService: OwnerService
+
     @Autowired
     lateinit var vetService: VetService
+
     @Autowired
     lateinit var petTypeService: PetTypeService
 
-    override fun run(vararg args: String?) {
+    @Autowired
+    lateinit var specialityService: SpecialityService
 
+    override fun run(vararg args: String?) {
+        if (petTypeService.findAll().isEmpty())
+           LoadData()
+    }
+
+    private fun LoadData() {
         val dog = PetType().apply { name = "dog" }.also { petTypeService.save(it) }
         val cat = PetType().apply { name = "cat" }.also { petTypeService.save(it) }
         println("Loaded pettypes...")
@@ -32,48 +39,60 @@ class DataLoader : CommandLineRunner {
             adress = "Peremoga street, 26"
             city = "Krivij Rig"
             telephone = "123456789"
-        }.also { it.pets.addAll(
+        }.also {
+            it.pets.addAll(
                 setOf((Pet().apply {
                     this.name = "Rich"
                     this.petType = dog
                     this.owner = it
                     this.birthday = LocalDate.now()
-                    }),
+                }),
                     (Pet().apply {
                         this.name = "Ron"
                         this.petType = dog
                         this.owner = it
                         this.birthday = LocalDate.now()
-                    }))
-        ) }.also { ownerService.save(it) }
+                    })
+                )
+            )
+        }.also { ownerService.save(it) }
         Owner().apply {
             firstName = "Fiona"
             lastName = "Glenanne"
             adress = "Darvin street, 2"
             city = "Zaporijja"
             telephone = "987654321"
-        }.also { it.pets.addAll(
-            setOf((Pet().apply {
-                this.name = "Motia"
-                this.petType = cat
-                this.owner = it
-                this.birthday = LocalDate.now()
-            }),
-                (Pet().apply {
-                    this.name = "Rygyi"
+        }.also {
+            it.pets.addAll(
+                setOf((Pet().apply {
+                    this.name = "Motia"
                     this.petType = cat
                     this.owner = it
                     this.birthday = LocalDate.now()
-                }))
-        ) }.also { ownerService.save(it) }
+                }),
+                    (Pet().apply {
+                        this.name = "Rygyi"
+                        this.petType = cat
+                        this.owner = it
+                        this.birthday = LocalDate.now()
+                    })
+                )
+            )
+        }.also { ownerService.save(it) }
         println("Loaded owners...")
+        val dentistry = Speciality().apply { description = "dentistry" }.also { specialityService.save(it) }
+        val surgery = Speciality().apply { description = "surgery" }.also { specialityService.save(it) }
+        val radiology = Speciality().apply { description = "radiology" }.also { specialityService.save(it) }
+        println("Loaded specialities...")
         Vet().apply {
             firstName = "Sam"
             lastName = "Axe"
+            specialities.addAll(setOf(dentistry, radiology))
         }.also { vetService.save(it) }
         Vet().apply {
             firstName = "John"
             lastName = "Silver"
+            specialities.addAll(setOf(surgery, radiology))
         }.also { vetService.save(it) }
         println("Loaded vets...")
     }
